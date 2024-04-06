ed0c1aa3ce08132ac6124c512b3d3bd6ab3e60b4
soujanyac <soujanya@yellowmessenger.com>
2024-04-06 18:29:21 +0530

    changes

--- controllers/friends.js
+++ controllers/friends.js
@@ -1,6 +1,6 @@
 const express = require('express');
 let router = express.Router();
-const {addFriendsToUser, deleteFriendsFromUser} = require('../models/User')
+const {addFriendsToUser, searchForNewFriends} = require('../models/User')
 const {sendFriendRequest, listFriendRequests, deleteFriendRequest} = require("../models/friendRequests");
 
 const acceptFriendRequest = async (fromUserId, toUserId) => {
@@ -13,7 +13,6 @@ const acceptFriendRequest = async (fromUserId, toUserId) => {
 };
 
 const rejectFriendRequest = async (fromUserId, toUserId) => {
-    await deleteFriendsFromUser({userId: toUserId, friendId: fromUserId})
     await deleteFriendRequest({fromUserId, toUserId})
 };
 
@@ -21,11 +20,27 @@ router.put('/add', async (req, res) => {
     const data = await sendFriendRequest({toUserId: req.body.toUserId, fromUserId: req.body.fromUserId});
     res.send(data);
 });
-router.put('/accept', acceptFriendRequest);
-router.put('/reject', rejectFriendRequest);
+router.post('/accept', async (req, res) => {
+    const toUserId = req.body.username
+    const fromUserId = req.body.friend_username
+    const data = await acceptFriendRequest(fromUserId, toUserId);
+    res.send(data);
+});
+router.post('/reject', async (req, res) => {
+    const toUserId = req.body.username
+    const fromUserId = req.body.friend_username
+    const data = await rejectFriendRequest(fromUserId, toUserId);
+    res.send(data);
+});
+
 router.post('/requests', async (req, res) => {
     let data = await listFriendRequests(req.body.username);
     res.send({requests: data});
+});
+
+router.post('/search', async (req, res) => {
+    let data = await searchForNewFriends(req.body.username);
+    res.send({friends: data});
 });
 
 router.post('/register', async (req, res) => {
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const {dbConnection} = require("../db/mongo");
 const _ = require('lodash');
+const {MongoClient} = require("mongodb");
 const userSchema = new mongoose.Schema(
     {
         userId: String,
@@ -29,9 +30,16 @@ const getIdFromUserName = async (username) => {
 };
 
 const addFriendsToUser = async ({fromUserId, toUserId}) => {
-    const user = await userDB.find({userId: toUserId});
-    user.friends = [...user.friends, fromUserId]
-    await user.save();
+    const {MongoClient} = require('mongodb')
+    const client = await MongoClient.connect(
+        'mongodb+srv://athena:athena123!@cluster-0.nexccgk.mongodb.net/'
+    );
+    const coll = client.db('gameskraft').collection('users');
+    const cursor = coll.find({userId: toUserId});
+    const result = await cursor.toObject();
+    await client.close();
+    cursor.friends = [...result.friends || [], fromUserId]
+    await cursor.save();
 };
 
 const deleteFriendsFromUser = async ({fromUserId, toUserId}) => {
@@ -40,6 +48,10 @@ const deleteFriendsFromUser = async ({fromUserId, toUserId}) => {
     user.friends = friends;
     await user.save();
 };
+
+const findFriends = async (userId) => {
+    // const data = await userDB.find({userId: })
+}
 
 module.exports = {userDB, getIdFromUserName, deleteFriendsFromUser, addFriendsToUser}
 
